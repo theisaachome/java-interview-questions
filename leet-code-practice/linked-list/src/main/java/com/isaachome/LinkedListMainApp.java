9103ccbe316bc7bd06402c4815724b0b49414f9a
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-07-23 13:00:20 +0800

    practiced LinkedListCycleDetection.

---
+++ leet-code-practice/linked-list/src/main/java/com/isaachome/LinkedListMainApp.java
@@ -0,0 +1,13 @@
+package com.isaachome;
+
+/**
+ * Hello world!
+ *
+ */
+public class LinkedListMainApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
